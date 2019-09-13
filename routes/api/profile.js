6358c50f0b0c6dc1eb7530bdcf2ea6569c1f4709
tunangst <trunangst@gmail.com>
2019-09-12 19:21:18 -0500

    added delete request for user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -149,4 +149,22 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// @route   DELETE api/profile
+// @desc    delete profile, user, & posts
+// @access  Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    //remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
