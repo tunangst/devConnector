9ac6a2fb874f3066bf3c6c89c182761464969771
tunangst <trunangst@gmail.com>
2019-09-12 19:10:23 -0500

    added some routing, profile modle, fixed mongoose depreciations, profile update/ get requests by user id

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -25,20 +27,21 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
     const { name, email, password } = req.body;
+
     try {
-      //see if user exists
-      let user = await User.findOne({ email: email });
+      let user = await User.findOne({ email });
       if (user) {
         return res
           .status(400)
           .json({ errors: [{ msg: "User already exists" }] });
       }
-      //if exists send error
-      //get user's gravatar
+
       const avatar = gravatar.url(email, {
         s: "200",
         r: "pg",
@@ -51,14 +54,26 @@ router.post(
         avatar,
         password
       });
-      //encript password using bcrypt
+
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
-      await user.save();
-      //return jsonwebtoken for instant relogin
 
-      console.log(req.body);
-      res.send("User registered");
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
