bbbe63df70cbef71660249c4a076382e1f97a8f6
tunangst <trunangst@gmail.com>
2019-09-14 14:11:01 -0500

    set up redux store and root reducer

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div>Login</div>;
+    //formData = setstate; setFormData = state
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = formData;
+
+    const onChange = element =>
+        setFormData({
+            ...formData,
+            [element.target.name]: element.target.value
+        });
+
+    const onSubmit = async element => {
+        element.preventDefault();
+        console.log('success');
+    };
+
+    return (
+        <Fragment>
+            <h1 className='large text-primary'>Sign In</h1>
+            <p className='lead'>
+                <i className='fas fa-user'></i> Sign Into Your Account
+            </p>
+            <form className='form' onSubmit={element => onSubmit(element)}>
+                <div className='form-group'>
+                    <input
+                        type='email'
+                        placeholder='Email Address'
+                        name='email'
+                        value={email}
+                        onChange={element => onChange(element)}
+                        required
+                    />
+                </div>
+                <div className='form-group'>
+                    <input
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        value={password}
+                        onChange={element => onChange(element)}
+                        minLength='6'
+                    />
+                </div>
+
+                <input
+                    type='submit'
+                    className='btn btn-primary'
+                    value='Login'
+                />
+            </form>
+            <p className='my-1'>
+                Don't have an account? <Link to='/register'>Sign Up</Link>
+            </p>
+        </Fragment>
+    );
 };
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,94 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
-  return <div>Register</div>;
+    //formData = setstate; setFormData = state
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    const onChange = element =>
+        setFormData({
+            ...formData,
+            [element.target.name]: element.target.value
+        });
+
+    const onSubmit = async element => {
+        element.preventDefault();
+        if (password !== password2) {
+            console.log('Passwords do not match');
+        } else {
+            console.log('success');
+        }
+    };
+
+    return (
+        <Fragment>
+            <h1 className='large text-primary'>Sign Up</h1>
+            <p className='lead'>
+                <i className='fas fa-user'></i> Create Your Account
+            </p>
+            <form className='form' onSubmit={element => onSubmit(element)}>
+                <div className='form-group'>
+                    <input
+                        type='text'
+                        placeholder='Name'
+                        name='name'
+                        value={name}
+                        onChange={element => onChange(element)}
+                        required
+                    />
+                </div>
+                <div className='form-group'>
+                    <input
+                        type='email'
+                        placeholder='Email Address'
+                        name='email'
+                        value={email}
+                        onChange={element => onChange(element)}
+                        required
+                    />
+                    <small className='form-text'>
+                        This site uses Gravatar so if you want a profile image,
+                        use a Gravatar email
+                    </small>
+                </div>
+                <div className='form-group'>
+                    <input
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        value={password}
+                        onChange={element => onChange(element)}
+                        minLength='6'
+                    />
+                </div>
+                <div className='form-group'>
+                    <input
+                        type='password'
+                        placeholder='Confirm Password'
+                        name='password2'
+                        value={password2}
+                        onChange={element => onChange(element)}
+                        minLength='6'
+                    />
+                </div>
+                <input
+                    type='submit'
+                    className='btn btn-primary'
+                    value='Register'
+                />
+            </form>
+            <p className='my-1'>
+                Already have an account? <Link to='/login'>Sign In</Link>
+            </p>
+        </Fragment>
+    );
 };
 
 export default Register;
